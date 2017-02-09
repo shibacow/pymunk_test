9ff25b1efe33ee9c763b6d96a190dc8d589d70d0
shibacow <shibacow@gmail.com>
2017-02-09 22:44:31 +0900

    ch data

--- 05.py
+++ 05.py
@@ -8,6 +8,7 @@ import pymunk.pygame_util
 import math
 import re
 from pymunk import vec2d
+import json
 
 def init():
     pygame.init()
@@ -18,6 +19,37 @@ def init():
     space.gravity = (0.0,0.0)
     return screen,clock,space
 
+
+
+def up_position(body,dt):
+    print("body={} dt={}".format(body,dt))
+def get_resource(space):
+    o=json.load(open('out.json'))
+    maxx=10
+    radius=14
+    #inetia = pymunk.moment_for_segment(maxx,0,radius,(0,0))
+    #body = pymunk.Body(mass,0,body_type=pymunk.Body.DYNAMIC)
+    body = pymunk.Body(10,10000)
+    #pa=pymunk.autogeometry.PolylineSet()
+    #body=pymunk.Body(body_type=pymunk.Body.STATIC)
+    body.position=(300,300)
+    body.velocity=vec2d.Vec2d(-100.0,0.0)
+    body.update_position=up_position
+    vec=[(float(x['x']),float(x['y'])) for x in o]
+    poly = pymunk.Poly(body,vec)
+    space.add(poly,body)
+    #for i,k in enumerate(o[:-1]):
+    #    p1=k
+    #    p2=o[i+1]
+     #   x1=float(p1['x'])
+     #   y1=float(p1['y'])
+     #   x2=float(p2['x'])
+     #   y2=float(p2['y'])
+        #pa.collect_segment((x1,y1),(x2,y2))
+        #l=pymunk.Segment(body,(x1,y1),(x2,y2),1)
+        #space.add(l)
+        #print("i={} k={}".format(i,k))
+    return poly
 def add_ball(space,x):
     mass=10
     radius=14
@@ -49,41 +81,10 @@ def static_ball(space,b):
 
 
 def add_joint(space,a,b):
-    #l = a.position.get_distance(b.position) * 0.9
-    #stiffness = 500.
-    #damping=10
-    #a0=10*math.pi/180.0
-    #b0=3500*math.pi/180.0
-    #print("a0={} b0={}".format(a0,b0))
-    #j2 = pymunk.constraint.RotaryLimitJoint(a,b, b0,a0)
-    #space.add(j2)
-
-    #j = pymunk.DampedSpring(a, b, (0,0), (0,0), rl, stiffness, damping)
-    #j.max_bias=100
-    #space.add(j)
-    #rest_angle=0
-    #j2 = pymunk.constraint.RotaryLimitJoint(a,b, 1.0,100.0)
-    #space.add(j2)
     j3 = pymunk.constraint.PinJoint(a,b,(0,0),(0,0))
     j3.collide_bodies=False
     j3.max_force=1000
     space.add(j3)
-    #j = pymunk.constraint.DampedRotarySpring(a,b,0,100,100)
-    #xdiff=a.position.x - b.position.x
-    #print("xdiff={}".format(xdiff))
-    #space.add(j)
-    #pass
-    #j4 = pymunk.constraint.SimpleMotor(a,b,0)
-    #j4.collide_bodies=False
-    #j4.max_force=1000
-    #space.add(j4)
-    #l = a.position.get_distance(b.position) * 0.9
-    #stiffness = 500.
-    #damping=200.
-    #j3 = pymunk.constraint.DampedSpring(a,b,(0,0),(0,0),l,stiffness,damping)
-    #j3.collide_bodies=False
-    #j3.max_force=1000
-    #space.add(j3)
 
 
 def show_binfo(i,b):
@@ -107,6 +108,14 @@ def show_v(i,b):
     av=b.angular_velocity
     print("i={} av={}".format(i,av))
 
+def move_it(space,x):
+    for b in space.bodies:
+        b.position.x=b.position.x-x
+    #for s in space.shapes:
+        #s.position.x=s.position.x-x
+        #print("shape={}".format(s))
+
+
 def main():
     (screen,clock,space) = init()
     draw_options = pymunk.pygame_util.DrawOptions(screen)
@@ -117,6 +126,7 @@ def main():
     add_ball(space,200)
     add_ball(space,150)
     add_ball(space,100)
+
 
     bodies=space.bodies
     bodies=sorted(bodies,key=lambda x:x.position.x,reverse=True)
@@ -129,6 +139,7 @@ def main():
     running=True
     is_interactive = False
     constraints = space.constraints
+    back_recouce=get_resource(space)
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -144,11 +155,12 @@ def main():
             bd = space.point_query_nearest(mouse_pos, 10, pymunk.ShapeFilter())
             if bd:
                 bd.shape.body.position=mouse_pos
-        #for i,b in enumerate(bodies):
-            #show_binfo(i,b)
-         #   show_v(i,b)
-       #for i,c in enumerate(constraints):
-        #    show_const(i,c)
+        #back_recouce.position.x=back_recouce.position.x - 10.0
+        #ax=back_recouce.position.x
+        #ay=back_recouce.position.y
+        #back_recouce.position.x=ax-100
+        #print("ax={} ay={}".format(ax,ay))
+        move_it(space,10)
         space.step(1/50.0)
         screen.fill((255,255,255))
         space.debug_draw(draw_options)
